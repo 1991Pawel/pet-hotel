9646d920192d56e767853442507efe3c8da5c879
Paweł Leśnik <lesnik.pawel1991@gmail.com>
2025-01-11 13:46:08 +0100

    add zod and rhf

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,3 +1,44 @@
+"use client";
+
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+const schema = z.object({
+  email: z.string().email("Wprowadź poprawny adres e-mail."),
+  password: z.string().min(6, "Hasło musi mieć co najmniej 6 znaków."),
+});
+
 export default function LoginPage() {
-  return <div>login page</div>;
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: zodResolver(schema),
+  });
+
+  const onSubmit = (data: any) => {
+    console.log(data, "informacje z formularza");
+  };
+
+  return (
+    <div>
+      <h2>Login</h2>
+      <form className="form" onSubmit={handleSubmit(onSubmit)}>
+        <div>
+          <label htmlFor="email">Email:</label>
+          <input {...register("email")} type="email" id="email" />
+        </div>
+        <div>
+          <label htmlFor="password">Password:</label>
+          <input {...register("password")} type="password" id="password" />
+        </div>
+
+        <button onClick={handleSubmit(onSubmit)} type="submit">
+          Log In
+        </button>
+      </form>
+    </div>
+  );
 }
--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -1,49 +1,43 @@
 "use client";
-import { ReactEventHandler, useState } from "react";
+
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+const schema = z.object({
+  email: z.string().email("Wprowadź poprawny adres e-mail."),
+  password: z.string().min(6, "Hasło musi mieć co najmniej 6 znaków."),
+});
 
 export default function RegisterPage() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [error, setError] = useState("");
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: zodResolver(schema),
+  });
 
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    if (!email || !password) {
-      setError("Both fields are required");
-      return;
-    }
-    // Handle login logic here, such as calling an API
-    console.log("Logged in with", email, password);
-    setError("");
+  const onSubmit = (data: any) => {
+    console.log(data, "informacje z formularza");
   };
 
   return (
     <div>
       <h2>Register</h2>
-      <form onSubmit={handleSubmit}>
-        {JSON.stringify({ email, password })}
+      <form className="form" onSubmit={handleSubmit(onSubmit)}>
         <div>
           <label htmlFor="email">Email:</label>
-          <input
-            type="email"
-            id="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-          />
+          <input {...register("email")} type="email" id="email" />
         </div>
         <div>
           <label htmlFor="password">Password:</label>
-          <input
-            type="password"
-            id="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
+          <input {...register("password")} type="password" id="password" />
         </div>
-        {error && <p style={{ color: "red" }}>{error}</p>}
-        <button type="submit">Log In</button>
+
+        <button onClick={handleSubmit(onSubmit)} type="submit">
+          Log In
+        </button>
       </form>
     </div>
   );
